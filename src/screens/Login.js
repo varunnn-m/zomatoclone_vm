c331d9d56a0971d92b43d06e42d12d7d1225356d
Varun Modi <modi78681@gmail.com>
2023-02-05 22:52:52 +0530

    commit otp screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,38 +4,81 @@ import { LOGIN_TITLE, THEME_COLOR } from "../strings.js"
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 import { TextInput } from 'react-native-gesture-handler'
 import auth from '@react-native-firebase/auth';
+import OTPInputView from '@twotalltotems/react-native-otp-input'
+import { set } from 'immer/dist/internal.js'
 
 const Login = () => {
 
-    const[mobileNum,setMobileNum]=useState('')
+    const [mobileNum, setMobileNum] = useState('')
+    const [confirm, setConfirm] = useState(null)
+    const [otp,setOtp]=useState("")
+    const [codeInput,setCodeInput]=useState("")
 
-    const signInWithPhoneNumber=async ()=> {
-        const confirmation = await auth().signInWithPhoneNumber("+91"+mobileNum);
-        //setConfirm(confirmation);
+    const signInWithPhoneNumber = async () => {
+        const confirmation = await auth().signInWithPhoneNumber("+91" + mobileNum);
+        setConfirm(confirmation);
         console.log(confirmation)
         console.log(mobileNum)
-        
+
     }
-    
+
 
     return (
         <View style={styles.container}>
-            <StatusBar backgroundColor={"transparent"} translucent={true} barStyle="light-content" />
+            <StatusBar backgroundColor={THEME_COLOR + "99"} translucent={false} barStyle="light-content" />
             <View style={styles.containerIn}>
                 <Image source={require('../images/BannerRed.png')} style={styles.banner} />
             </View>
             <Text style={styles.loginTitle}>{LOGIN_TITLE}</Text>
-            <View style={styles.divider}>
-                <View style={[styles.dividerView, { marginLeft: 20 }]}>
-                </View>
-                <Text style={styles.dividerText}>Login or Signup</Text>
-                <View style={[styles.dividerView, { marginRight: 20 }]}>
-                </View>
-            </View>
-            <TextInput placeholder='mobile number' keyboardType='number-pad' style={styles.mobileInput} onChangeText={setMobileNum} />
-            <TouchableOpacity style={styles.login} onPress={signInWithPhoneNumber}>
-                <Text style={{ fontWeight: "500", color: "white" }}>Login with OTP</Text>
-            </TouchableOpacity>
+
+            {
+                confirm!== null ?
+                    <View>
+                        <View style={styles.divider}>
+                            <View style={[styles.dividerView, { marginLeft: 20 }]}>
+                            </View>
+                            <Text style={styles.dividerText}>Login or Signup</Text>
+                            <View style={[styles.dividerView, { marginRight: 20 }]}>
+                            </View>
+                        </View>
+                        <TextInput placeholder='mobile number' keyboardType='number-pad' style={styles.mobileInput} onChangeText={setMobileNum} />
+                        <TouchableOpacity style={[styles.login,{backgroundColor:mobileNum.length!==10?"grey":THEME_COLOR}]} onPress={signInWithPhoneNumber}>
+                            <Text style={{ fontWeight: "500", color: "white" }}>Login with OTP</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    :
+                    <View>
+                        <View style={styles.divider}>
+                            <View style={[styles.dividerView, { marginLeft: 20 }]}>
+                            </View>
+                            <Text style={styles.dividerText}>Enter OTP</Text>
+                            <View style={[styles.dividerView, { marginRight: 20 }]}>
+                            </View>
+                        </View>
+
+                        <OTPInputView
+                            style={{ width: '80%', height: 85,alignSelf:"center",backgroundColor:"#fff5f5" }}
+                            pinCount={6}
+
+                            onCodeChanged = {code => {setCodeInput(code)}}
+                            autoFocusOnLoad
+                            codeInputFieldStyle={styles.underlineStyleBase}
+                            codeInputHighlightStyle={styles.underlineStyleHighLighted}
+                            onCodeFilled={(code => {
+                                console.log(`Code is ${code}, you are good to go!`)
+                                setOtp(code)
+                            })}
+
+                        />
+                        <TouchableOpacity style={[styles.login,{backgroundColor:codeInput.length<6?"grey":THEME_COLOR}]} onPress={signInWithPhoneNumber} disabled={codeInput.length<6}>
+                            <Text style={{ fontWeight: "500", color: "white" }}>Verify OTP</Text>
+                        </TouchableOpacity>
+
+
+
+                    </View>
+            }
         </View>
     )
 }
@@ -93,7 +136,6 @@ const styles = StyleSheet.create({
 
     },
     login: {
-        backgroundColor: THEME_COLOR,
         width: responsiveWidth(90),
         borderRadius: 10,
         height: responsiveHeight(6),
@@ -101,5 +143,18 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignSelf: "center",
         marginTop: responsiveHeight(3.7)
-    }
+    },
+    
+      underlineStyleBase: {
+        width: "140%",
+        height: "50%",
+        borderWidth: 1,
+        borderColor:"#00000099",
+        borderRadius:10,
+        backgroundColor:"white"
+      },
+    
+      underlineStyleHighLighted: {
+        borderColor: THEME_COLOR,
+      }
 })
